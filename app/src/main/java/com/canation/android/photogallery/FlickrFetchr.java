e76991641f8b68b6dab97fdf94f11bb4ec0453d3
Kent <romeobidien@gmail.com>
2017-03-19 01:30:14 +0700

    Chapter 25 : HTTP and Background Tasks       parse JSON String to GalleryItems list

--- app/src/main/java/com/canation/android/photogallery/FlickrFetchr.java
+++ app/src/main/java/com/canation/android/photogallery/FlickrFetchr.java
@@ -3,11 +3,17 @@ package com.canation.android.photogallery;
 import android.net.Uri;
 import android.util.Log;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by CangNguyen on 3/18/2017.
@@ -44,7 +50,9 @@ public class FlickrFetchr {
         return new String(getUrlBytes(urlSpec));
     }
 
-    public void fetchItems() {
+    public List<GalleryItem> fetchItems() {
+        List<GalleryItem> items = new ArrayList<>();
+
         try {
             String url = Uri.parse("https://api.flickr.com/services/rest/")
                     .buildUpon()
@@ -57,8 +65,35 @@ public class FlickrFetchr {
 
             String jsonString = getUrlString(url);
             Log.i(TAG, "Received JSON: " + jsonString);
+            JSONObject jsonBody = new JSONObject(jsonString);
+            parseItems(items, jsonBody);
         } catch (IOException ioe) {
             Log.e(TAG, "Failed to fetch items", ioe);
+        } catch (JSONException e) {
+            Log.e(TAG, "Failed to parse Json", e);
+        }
+
+        return items;
+    }
+
+    private void parseItems(List<GalleryItem> items, JSONObject jsonBody) throws IOException, JSONException{
+        JSONObject photosJsonObject = jsonBody.getJSONObject("photos");
+        JSONArray photoJsonArray = photosJsonObject.getJSONArray("photo");
+
+        for (int i = 0; i < photoJsonArray.length(); i++) {
+            JSONObject photoJsonObject = photoJsonArray.getJSONObject(i);
+
+            GalleryItem item = new GalleryItem();
+            item.setId(photoJsonObject.getString("id"));
+            item.setCaption(photoJsonObject.getString("title"));
+
+            if (!photoJsonObject.has("url_s")) {
+                continue;
+            }
+
+            item.setUrl(photoJsonObject.getString("url_s"));
+
+            items.add(item);
         }
     }
 }
---
+++ app/src/main/java/com/canation/android/photogallery/GalleryItem.java
@@ -0,0 +1,40 @@
+package com.canation.android.photogallery;
+
+/**
+ * Created by CangNguyen on 3/19/2017.
+ */
+
+public class GalleryItem {
+    private String mCaption;
+    private String mId;
+    private String mUrl;
+
+    @Override
+    public String toString() {
+        return mCaption;
+    }
+
+    public String getCaption() {
+        return mCaption;
+    }
+
+    public void setCaption(String caption) {
+        mCaption = caption;
+    }
+
+    public String getId() {
+        return mId;
+    }
+
+    public void setId(String id) {
+        mId = id;
+    }
+
+    public String getUrl() {
+        return mUrl;
+    }
+
+    public void setUrl(String url) {
+        mUrl = url;
+    }
+}
