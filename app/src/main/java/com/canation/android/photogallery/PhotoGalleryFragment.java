e7dc19d95f1d2e07ac7d8f3bd51810f86aa50db3
Kent <romeobidien@gmail.com>
2017-03-19 00:33:32 +0700

    Chapter 25 : HTTP and Background Tasks       fetch json from flickr

--- app/src/main/java/com/canation/android/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/canation/android/photogallery/PhotoGalleryFragment.java
@@ -1,19 +1,25 @@
 package com.canation.android.photogallery;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.io.IOException;
 
 /**
  * Created by CangNguyen on 3/18/2017.
  */
 
 public class PhotoGalleryFragment extends Fragment {
+    private static final String TAG = "PhotoGalleryFragment";
+
     private RecyclerView mPhotoRecyclerView;
 
     public static PhotoGalleryFragment newInstance() {
@@ -24,6 +30,7 @@ public class PhotoGalleryFragment extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRetainInstance(true);
+        new FetchItemsTask().execute();
     }
 
     @Nullable
@@ -35,5 +42,14 @@ public class PhotoGalleryFragment extends Fragment {
         mPhotoRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
 
         return view;
+    }
+
+    private class FetchItemsTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            new FlickrFetchr().fetchItems();
+            return null;
+        }
     }
 }
