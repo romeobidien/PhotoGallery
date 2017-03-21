6d640ef0f41e9b04b3428a4d472823675c5620bf
Kent <romeobidien@gmail.com>
2017-03-21 21:14:09 +0700

    Chapter 27 : Search using SearchView : completed

---
+++ app/src/main/java/com/canation/android/photogallery/QueryPreferences.java
@@ -0,0 +1,24 @@
+package com.canation.android.photogallery;
+
+import android.content.Context;
+import android.preference.PreferenceManager;
+
+/**
+ * Created by CangNguyen on 3/21/2017.
+ */
+
+public class QueryPreferences {
+    private static final String PREF_SEARCH_QUERY = "searchQuery";
+
+    public static String getStoredQuery(Context context) {
+        return PreferenceManager.getDefaultSharedPreferences(context)
+                .getString(PREF_SEARCH_QUERY, null);
+    }
+
+    public static void setStoredQuery(Context context, String query) {
+        PreferenceManager.getDefaultSharedPreferences(context)
+                .edit()
+                .putString(PREF_SEARCH_QUERY, query)
+                .apply();
+    }
+}
